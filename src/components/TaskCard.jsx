f7392ec2531024441c479b881f8864323dd995cd
Alfred Tuva <muindetuva@gmail.com>
2025-09-29 18:09:44 +0300

    feat: show task completed state

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,9 +1,9 @@
 function TaskCard({ task }) {
   return (
-    <div className="w-lg mx-auto my-2 bg-primary text-white p-4 rounded shadow-sm">
+    <div className="w-lg mx-auto my-2 bg-primary text-white p-4 rounded shadow-sm flex justify-between items-center">
       <p>{task.title}</p>
       <span>
-        {/* { task.completed ? '✅' : '❌'} */}
+        { task.completed ? '✅ Done' : '❌ Pending'}
       </span>
     </div>
   );
