668ee08b6c4de8c08da2e28adb1cf91b3d9e2555
Alfred Tuva <muindetuva@gmail.com>
2025-09-29 16:22:27 +0300

    feat: implement routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router'
+import router from './router/router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>Hello there, we're about to build a project</h1>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
